f03024d86a57a1eef01ebbc512586dcf724a928d
Sam <mirthquakes@gmail.com>
2013-01-20 18:51:56 -0600

    Try to push entire database?

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -34,7 +34,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
+            'NAME': os.path.join('/Users/sam/Sites/django/data/', 'sqlite3.db'),  # Or path to database file if using sqlite3.
             'USER': '',                      # Not used with sqlite3.
             'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
