961a1b078d62f8ce27c6ae254ede2af5375152a0
Sam <mirthquakes@gmail.com>
2013-01-20 18:41:55 -0600

    Toying with admin/public media prefixes...

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -82,7 +82,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/media/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
